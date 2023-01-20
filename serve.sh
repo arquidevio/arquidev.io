e78d1559478e012411e7ebefee0326b7c415944e
queil <queil@users.noreply.github.com>
2023-01-20 14:03:52 +0000

    Playing with layout

--- serve.sh
+++ serve.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PORT=8090
+fuser -k $PORT/tcp
 busybox httpd -p $PORT && \
-echo "Listening on http://localhost:$PORT" && \
+echo "Listening on http://localhost:$PORT"
 echo "Use: fuser -k $PORT/tcp to exit"
