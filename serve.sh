ee50d387960e2a8e775f7591554aeb2a64313c24
queil <queil@users.noreply.github.com>
2023-02-02 19:04:21 +0000

    fixes: menu now closes; background fix for iPhone + some more styling

--- serve.sh
+++ serve.sh
@@ -2,6 +2,6 @@
 
 PORT=8090
 fuser -k $PORT/tcp
-busybox httpd -p $PORT && \
+busybox httpd -p $PORT -c httpd.conf && \
 echo "Listening on http://localhost:$PORT"
 echo "Use: fuser -k $PORT/tcp to exit"
